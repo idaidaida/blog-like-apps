7ff128538748476e9f3a2a6cad8243420bc86ffb
yuhei-ida <yuuhei0311>
2019-06-30 22:58:04 +0900

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "blogs#index"
   resources :blogs do
-    resources :entries do
-      resources :comments do
+    resources :entries, only:[:show,:new,:edit,:create,:update] do
+      resources :comments, only:[:create,:destroy] do
         patch :approve, on: :member
       end
     end
